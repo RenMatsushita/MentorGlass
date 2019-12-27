73965c5c011e1e5ac8b8283661f2c1b1e98f7fb5
RenMatsushita <ren.git.x@gmail.com>
2019-12-27 09:11:05 +0900

    init

---
+++ MentorGlass/Views/MentorGlassViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MentorGlass
+//
+//  Created by Ren Matsushita on 2019/12/26.
+//  Copyright © 2019 Ren Matsushita. All rights reserved.
+//
+
+import UIKit
+
+class MentorGlassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
